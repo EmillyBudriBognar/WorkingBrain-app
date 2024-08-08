13ae9548276042ca55b0db781303129c79661e5d
Michelle Budri Bognar <emillybudribognar@gmail.com>
2024-08-08 18:19:17 -0300

    criando o loading

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import { Loading } from '@/components/loading';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>oi</Text>
+    <View style={{flex:1, alignItems: 'center', backgroundColor: '#fff', paddingTop: 50}}>
+      <Text style={{color: 'blue', fontSize: 30, textAlign: 'center'}}>Hello World 😎</Text>
+      <Loading/>
     </View>
   );
 }
