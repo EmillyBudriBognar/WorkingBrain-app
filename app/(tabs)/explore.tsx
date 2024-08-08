89cacc610065f33136bd3af410b1322c95b3be6a
Michelle Budri Bognar <emillybudribognar@gmail.com>
2024-08-08 17:48:44 -0300

    iniciando o projeto

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Text, View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>
+        2 view
+      </Text>
+    </View>
+  );
+}
