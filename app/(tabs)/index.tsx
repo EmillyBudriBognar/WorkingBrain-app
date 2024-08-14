114be04c01292a21b1d23b44c35c3b69c856330e
Emilly Budri <emillybudribognar@gmail.com>
2024-08-13 23:46:48 -0300

    teste

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Loading } from '@/components/loading';
 export default function HomeScreen() {
   return (
     <View style={{flex:1, alignItems: 'center', backgroundColor: '#fff', paddingTop: 50}}>
-      <Text style={{color: 'blue', fontSize: 30, textAlign: 'center'}}>Hello World 😎</Text>
+      <Text style={{color: 'blue', fontSize: 30, textAlign: 'center'}}>Hello World! 😎</Text>
       <Loading/>
     </View>
   );
